ec8099c35c95885e572f790b315a8e0f755d77a8
hideoutbush <773903921@qq.com>
2023-11-15 12:54:07 +0800

    初始commit

---
+++ Source/MMDDemo/MMDDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MMDDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MMDDEMO_API AMMDDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/MMDDemoEditor.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class MMDDemoEditorTarget : TargetRules
+{
+	public MMDDemoEditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		ExtraModuleNames.Add("MMDDemo");
+	}
+}
